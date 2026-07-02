f3360713303011b609f4cfd2707fd8b438a8c5ee
GabrielBuenoSathler <garouthehumanmonster@protonmail.com>
2026-07-02 18:07:50 -0300

    adicionando guru

--- src/freelancer_crawler/crawler.py
+++ src/freelancer_crawler/crawler.py
@@ -89,6 +89,28 @@ with sync_playwright() as p:
                 "plataforma": "Freelancer"
             })
 
+    # -------------------------
+    # Guru.com
+    # -------------------------
+    page.goto("https://www.guru.com/d/jobs/c/programming-development/")
+    page.wait_for_selector("h2.jobRecord__title")
+
+    soup = BeautifulSoup(page.content(), 'html.parser')
+
+    for h2 in soup.select("h2.jobRecord__title"):
+        a = h2.find("a", href=True)
+        titulo = h2.get_text(strip=True)
+
+        if titulo and a:
+            href = a["href"]
+            link = href if href.startswith("http") else "https://www.guru.com" + href
+
+            jobs.append({
+                "titulo": titulo,
+                "link": link,
+                "plataforma": "Guru"
+            })
+
     browser.close()
 
 # -------------------------
